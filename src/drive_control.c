d5c8239d09143085b3eeffc0ec06df2aae6b265c
Garth Verdeflor <garthverdeflor@gmail.com>
2022-02-28 09:34:50 -0500

    Created Makefile for /src

---
+++ src/drive_control.c
@@ -0,0 +1,19 @@
+/* drive_control.c ---
+ * 
+ *
+ * Author: Suraj Srivats, Garth Verdeflor
+ * Created: Sat Feb 26 11:33:29 2022 (-0400)
+ * Version: 1.0  
+ *
+ *
+ * Description: Implements drive control algorithm.
+ *
+ */
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
